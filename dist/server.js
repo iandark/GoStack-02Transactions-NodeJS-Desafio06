7ca5572f875f8a2d8931b640ef881fb177516d63
Derkian Souza <derkian@iands.com.br>
2020-12-13 15:25:36 -0300

    fix: Ajuste de gitignore

---
+++ dist/server.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv_1 = __importDefault(require("dotenv"));
+var app_1 = __importDefault(require("./app"));
+dotenv_1.default.config();
+app_1.default.listen(3333, function () {
+    console.log('🚀 Server started on port 3333!');
+});
+//# sourceMappingURL=server.js.map
